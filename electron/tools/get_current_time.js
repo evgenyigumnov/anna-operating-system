402efd3fb5869ff8fd497983557fc357b3809d53
Evgeny Igumnov <igumnovnsk@gmail.com>
2026-03-16 16:30:01 +0500

    tools

--- electron/tools/get_current_time.js
+++ electron/tools/get_current_time.js
@@ -2,7 +2,7 @@ module.exports = {
   definition: {
     type: 'function',
     function: {
-      name: 'get_current_time',
+      name: 'get_cДurrent_time',
       description: 'Returns the current local time for the Electron app runtime.',
       parameters: {
         type: 'object',
