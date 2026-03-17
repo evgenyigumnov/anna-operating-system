f2fe4a38981240b85df167c11ae48877c007166d
Evgeny Igumnov <igumnovnsk@gmail.com>
2026-03-17 12:39:16 +0500

    Rules

--- electron/identity.js
+++ electron/identity.js
@@ -8,16 +8,22 @@ const {
 const DEFAULT_IDENTITY = {
   name: 'Anna',
   sex: 'Female',
+  language: 'English',
   style: 'Simple, concise, and with a sense of humor',
   rules: '',
+  operatingSystem: '',
 };
 
 const HEADING_TO_FIELD = new Map([
   ['name', 'name'],
   ['sex', 'sex'],
   ['gender', 'sex'],
+  ['language', 'language'],
+  ['locale', 'language'],
   ['style', 'style'],
   ['rules', 'rules'],
+  ['operating system', 'operatingSystem'],
+  ['os', 'operatingSystem'],
 ]);
 
 function parseIdentityMarkdown(markdown) {
@@ -40,17 +46,23 @@ function buildIdentityPrompt(identity) {
     ...(identity && typeof identity === 'object' ? identity : {}),
   };
   const normalizedRules = normalizeValue(normalizedIdentity.rules, '');
+  const normalizedOperatingSystem = normalizeValue(normalizedIdentity.operatingSystem, '');
 
   const promptLines = [
     'Identity profile:',
     `Name: ${normalizeValue(normalizedIdentity.name, DEFAULT_IDENTITY.name)}`,
     `Sex: ${normalizeValue(normalizedIdentity.sex, DEFAULT_IDENTITY.sex)}`,
+    `Language: ${normalizeValue(normalizedIdentity.language, DEFAULT_IDENTITY.language)}`,
     `Style: ${normalizeValue(normalizedIdentity.style, DEFAULT_IDENTITY.style)}`,
   ];
 
   if (normalizedRules) {
     promptLines.push('Rules:');
     promptLines.push(normalizedRules);
+  }
+
+  if (normalizedOperatingSystem) {
+    promptLines.push(`Operating System: ${normalizedOperatingSystem}`);
   }
 
   return promptLines.join('\n');
